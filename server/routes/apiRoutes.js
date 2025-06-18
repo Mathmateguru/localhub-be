c453c1dbd1e673dd6da2410eed5eceda0dbd5ca6
hnobi <hnobi08@yahoo.com>
2025-06-18 14:25:38 +0100

    move routes to apiroute file

---
+++ server/routes/apiRoutes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { createCommunity, getCommunties, getSingleCommunity, updateCommunity, deleteCommunity} from '../controllers/community.js'
+import { createPost, getPost, getSinglePost, updatePost, deletePost} from '../controllers/post.js'
+import { createUser, getSingleUser, getUsers, updateUser, deleteUser} from '../controllers/user.js'
+
+const router = Router()
+
+router.post('/community', createCommunity)
+      .get('/community', getCommunties);
+
+router.get('/community/:id', getSingleCommunity)
+      .put('/community/:id', updateCommunity)
+      .delete('/community/:id', deleteCommunity);
+
+router.post('/post', createPost)
+      .get('/post', getPost);
+
+router.get('/post/:id', getSinglePost)
+      .get('/post/:id', updatePost)
+      .delete('/post/:id', deletePost);
+
+
+router.post('/user', createUser)
+      .get('/user', getUsers);
+
+router.get('/user/:id', getSingleUser)
+      .put('/user/:id', updateUser)
+      .delete('/user/:id', deleteUser);
+
+
+
+export default router
