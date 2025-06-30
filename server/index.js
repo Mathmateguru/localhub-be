c7c6a9e6bba2f7b4daefb82fcaae429bc072ed35
hnobi <hnobi08@yahoo.com>
2025-06-30 14:46:08 +0100

    fix cors error

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ import express from 'express'
 import connectDB from './db.js'
 import dotenv from 'dotenv'
 import router from './routes/apiRoutes.js'
+import cors from 'cors'
 dotenv.config()
 const app = express()
 
 const port = process.env. PORT || 4000
 connectDB()
 
-
+app.use(cors())
 app.use(express.json()) // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })) // Middleware to parse URL-encoded bodies
 app.get('/', (req, res) => {
